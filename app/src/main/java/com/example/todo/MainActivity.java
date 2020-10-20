db002d415585739a2567c3d4d71abc71bedc0ffb
Vadym <vadikdeputat@gmail.com>
2020-10-20 15:31:32 +0200

    add method onOptionsItemSelected

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.todo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -50,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
         icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
 
         return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add_new_task) {
+            final EditText userTaskGet = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Add new task")
+                    .setMessage("What are you wont add?")
+                    .setView(userTaskGet).
+                            setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String task = String.valueOf(userTaskGet.getText());
+                                    dbHelper.insertData(task);
+                                    loadAllTasks();
+                                }
+                            })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
