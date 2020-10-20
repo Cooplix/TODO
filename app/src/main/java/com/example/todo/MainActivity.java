a8f696bcc33dc9440e3f8f4901234b5f91ec50aa
Vadym <vadikdeputat@gmail.com>
2020-10-20 15:13:30 +0200

    add method loadAllTasks for update Data

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBase dbHelper;
+    private  ListView all_tasks;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DataBase(this);
+        all_tasks = findViewById(R.id.list_tasks);
+
+        loadAllTasks();
+    }
+
+    private void loadAllTasks() {
+        ArrayList<String> taskList = dbHelper.getAllTasks();
+        if(adapter == null) {
+            adapter = new ArrayAdapter<String>(this, R.layout.row, R.id.list_tasks, taskList);
+            all_tasks.setAdapter(adapter);
+        } else {
+            adapter.clear();
+            adapter.addAll(taskList);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
